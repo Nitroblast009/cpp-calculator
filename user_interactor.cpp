e6f8fa874b45b78dc0ebcfbb926eb82b2efcf0d5
Faizoo <faizaan3424@gmail.com>
2021-01-31 20:03:39 -0500

    get user numbers

--- user_interactor.cpp
+++ user_interactor.cpp
@@ -1,7 +1,25 @@
 #include "calculator.cpp"
 
+double getUserOperand(std::string message) {
+    double userOperand;
+    std::cout << message;
+    std::cin >> userOperand;
+    return userOperand;
+}
+
+char getUserOperationType() {
+    char userOperationType;
+    std::cout << "Which operation would you like to perform (+ for addition, - for subtraction, * for multiplication, / for division)? ";
+    std::cin >> userOperationType;
+    return userOperationType;
+}
+
 int main() {
-    hi();
-    std::cout << "otnhuteonhunotue";
+    while(true)   {
+        double num1 = getUserOperand("\nWhat is the first number you would like to use? ");
+        double num2 = getUserOperand("What is the second number you would like to use? ");
+        switch(getUserOperationType()) { }
+        break;
+    }
     return EXIT_SUCCESS;
 }
